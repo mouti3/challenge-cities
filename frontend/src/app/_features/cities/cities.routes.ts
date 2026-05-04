edba7bbcc7ea9ea8c32551ef79dfd5e5bf81df5a
mouti3 <benabedamoutipr@gmail.com>
2026-05-04 23:51:05 +0200

    setup frontend repo with primeng and bootsrap

---
+++ frontend/src/app/_features/cities/cities.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+export const CITIES_ROUTES: Routes = [{ path: '', component: HomeComponent }];
